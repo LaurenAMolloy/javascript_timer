98e9b5f9822db1b7d1bfc2b0d184ee6b42530847
lauren_m <laurenamolloy@gmail.com>
2025-09-02 19:19:19 +0300

    Implemented timer class functionality

--- app.js
+++ app.js
@@ -31,6 +31,17 @@ console.log("HI THERE");
 // #2 Did you bind call or apply? It is the first argument passed in
 // # 3 Where did we invoke the function?
 
+//How do we store the data? Where does current time live?
+//Option 1
+//An instance variable? 
+//Option 2
+//Current time will live in the input
+//We will take the value from the input
+//Why would we do that?
+//It makes UX easier to handle
+//We do not need an event listener
+//Storing data in the dom
+
 class Timer {
     //special function that is called automatically
     //When a new timer is created
@@ -43,14 +54,64 @@ class Timer {
 
         //add event listener to the start button
         //bind event listeners to our elements
-        this.startBtn.addEventListener("click", this.start)
+        this.startBtn.addEventListener("click", this.start);
+        this.pauseBtn.addEventListener("click", this.pause);
     }
     //This arrow function is moved into the constructor under the hood so it solves our problem
     start = () => {
         //The value of this is the button!
         //It has been overwritten
         //console.log(this);
+        //Call .tick every second
+        this.tick();
+        this.intervalId = setInterval(this.tick, 1000);
+        
+    };
+
+    pause = () => {
+        //get the timer variable
+        console.log("paused")
+        clearInterval(this.intervalId);
+    };
+
+    //tick method
+    //what do we want to run every second?
+    tick = () => {
+        if (this.timeRemaining <= 0){
+            this.pause();
+        } else {
+            this.timeRemaining = timeRemaining - 1;
+        }
+        //console.log("tick");
+        //look into the dom
+        //decrease the value
+        //pull the value and get a number not a string
+        //This is our one source of truth
+        // const timeRemaining = parseFloat(this.durationInput.value);
+        //The right hand side if this expression is the argument for the setter
+        //this.timeRemaining = timeRemaining - 1;
+        //console.log(timeRemaining)
     }
+
+    //this is treated like an instance variable
+    //we don't have to call a mathod when we do this
+    //it makes it clear to other engineers we are just getting value from a variable
+    //We have hidden the complexity
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time
+    }
+
+    //getters and setters
+    // getTime(){
+    //     return parseFloat(this.durationInput.value)
+    // }
+    // setTime (time){
+    //     this.durationInput.value = time;
+    // }
 }
 
 const durationInput = document.querySelector("#duration");
